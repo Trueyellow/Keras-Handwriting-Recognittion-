1152de7622fad7c6ff2bf76af163bd00a0503f73
HansZone <wangyehan@outlook.com>
2017-03-30 22:34:05 -0400

    Update extract_character(adaptive).py

--- Image Processing/extract_character(adaptive).py
+++ Image Processing/extract_character(adaptive).py
@@ -15,6 +15,59 @@ dilated = cv2.dilate(gauss, kernel, iterations=1)  # dilate"""
 close = cv2.erode(dilated, kernel, iterations=4)
 open = cv2.dilate(erosion, kernel, iterations=3)
 
+# -------------- cut function --------------------
+def cut(gray):
+    _, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+
+    binary=np.array(binary)
+    print(binary.shape)
+    hist_x = np.zeros((gray.shape[0], 1))
+    hist_y = np.zeros((gray.shape[1], 1))
+
+    for i in range(binary.shape[0]):
+        counter = 0
+        for j in range(binary.shape[1]):
+            if binary[i][j] == 0:
+                counter += 1
+        hist_x[i] = counter
+
+    for m in range(binary.shape[1]):
+        counter = 0
+        for n in range(binary.shape[0]):
+            if binary[n][m] == 0:
+                counter += 1
+        hist_y[m] = counter
+
+    left = right = up = down = 0
+
+    for a in range(binary.shape[0]):
+        if hist_x[a] != 0:
+            up = a
+            break
+
+    for b in reversed(range(binary.shape[0])):
+        if hist_x[b] != 0:
+            down = b
+            break
+
+    for c in range(binary.shape[1]):
+        if hist_y[c] != 0:
+            left = c
+            break
+
+    for d in reversed(range(binary.shape[1])):
+        if hist_y[d] != 0:
+            right = d
+            break
+
+    if left - 3 >= 0 and right + 3 < binary.shape[1] and up - 3 >= 0 and down + 3 < binary.shape[0]:
+        new_image = gray[up - 3:down + 3, left - 3:right + 3]
+    else:
+        new_image = gray[up:down, left:right]
+
+    return new_image
+
+
 _, contours, hierarchy = cv2.findContours(binary_erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # get contours
 
 d = 1
@@ -47,12 +100,14 @@ for i in range(len(contour_list)):
     [y, x, w, h] = contour_i
     contour_list = np.delete(contour_list, min_index, 0)
 
-    if x-15>0 and x+h+2<gray1.shape[0] and y-3>0 and y+w+3<gray1.shape[1]:
+    if x-80>0 and x+h+2<gray1.shape[0] and y-3>0 and y+w+3<gray1.shape[1]:
         test = gray1[x - 80:x + h + 2, y - 3:y + w + 3]
-    elif x-15<0 and x+h+2<gray1.shape[0] and y-3>0 and y+w+3<gray1.shape[1]:
+    elif x-80<=0 and x+h+2<gray1.shape[0] and y-3>0 and y+w+3<gray1.shape[1]:
         test=gray1[0:x+h,y:y+w]
     else:
-        test=gray[x:x+h,y:y+w]
+        test=gray1[x:x+h,y:y+w]
+
+    test=cut(test)
 
     cv2.imwrite("cd{}.jpg".format(d), test)
     d = d + 1
